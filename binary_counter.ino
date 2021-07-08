a84cb3378dc9d5e7cea376a43195d70b79b231be
Kwasi  Addo Nyarko <60752133+Kan-j@users.noreply.github.com>
2021-07-08 18:01:15 +0000

    Add files via upload

---
+++ binary_counter.ino
@@ -0,0 +1,143 @@
+
+int pin1 = 13;
+int pin2 = 11;
+int pin3 = 9;
+int pin4 = 7;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ digitalWrite(pin1, LOW);
+ digitalWrite(pin2, LOW);
+ digitalWrite(pin3, LOW);
+ digitalWrite(pin4, LOW);
+ delay(1000);
+
+ 
+ digitalWrite(pin1, LOW);
+ digitalWrite(pin2, LOW);
+ digitalWrite(pin3, LOW);
+ digitalWrite(pin4, HIGH);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, LOW);
+ digitalWrite(pin2, LOW);
+ digitalWrite(pin3, HIGH);
+ digitalWrite(pin4, LOW);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, LOW);
+ digitalWrite(pin2, LOW);
+ digitalWrite(pin3, HIGH);
+ digitalWrite(pin4, HIGH);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, LOW);
+ digitalWrite(pin2, HIGH);
+ digitalWrite(pin3, LOW);
+ digitalWrite(pin4, LOW);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, LOW);
+ digitalWrite(pin2, HIGH);
+ digitalWrite(pin3, LOW);
+ digitalWrite(pin4, HIGH);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, LOW);
+ digitalWrite(pin2, HIGH);
+ digitalWrite(pin3, HIGH);
+ digitalWrite(pin4, LOW);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, LOW);
+ digitalWrite(pin2, HIGH);
+ digitalWrite(pin3, HIGH);
+ digitalWrite(pin4, HIGH);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, HIGH);
+ digitalWrite(pin2, LOW);
+ digitalWrite(pin3, LOW);
+ digitalWrite(pin4, LOW);
+ delay(1000);
+ 
+ digitalWrite(pin1, HIGH);
+ digitalWrite(pin2, LOW);
+ digitalWrite(pin3, LOW);
+ digitalWrite(pin4, HIGH);
+ delay(1000);
+
+ 
+ digitalWrite(pin1, HIGH);
+ digitalWrite(pin2, LOW);
+ digitalWrite(pin3, HIGH);
+ digitalWrite(pin4, LOW);
+ delay(1000);
+
+ 
+ digitalWrite(pin1, HIGH);
+ digitalWrite(pin2, LOW);
+ digitalWrite(pin3, HIGH);
+ digitalWrite(pin4, HIGH);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, HIGH);
+ digitalWrite(pin2, HIGH);
+ digitalWrite(pin3, LOW);
+ digitalWrite(pin4, LOW);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, HIGH);
+ digitalWrite(pin2, HIGH);
+ digitalWrite(pin3, LOW);
+ digitalWrite(pin4, HIGH);
+ delay(1000);
+
+
+ 
+ digitalWrite(pin1, HIGH);
+ digitalWrite(pin2, HIGH);
+ digitalWrite(pin3, HIGH);
+ digitalWrite(pin4, LOW);
+ delay(1000);
+
+ 
+ digitalWrite(pin1, HIGH);
+ digitalWrite(pin2, HIGH);
+ digitalWrite(pin3, HIGH);
+ digitalWrite(pin4, HIGH);
+ delay(1000);
+
+ 
+
+ 
+}
